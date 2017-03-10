217db43e64b4a2198f674a2f5873ad5f1f47ad51
Aleksey Subbotenko <asu@dvhb.ru>
2017-03-10 17:53:16 +0300

    Core: Log build webpack stats

--- scripts/build.js
+++ scripts/build.js
@@ -3,12 +3,19 @@
 const webpack = require('webpack');
 const makeWebpackConfig = require('./make-webpack-config');
 const gulpRunner = require('../gulp/gulp-runner');
+const colorsSupported = require('supports-color');
 
 module.exports = function build(config, callback) {
     let env = 'production';
 
     return webpack(makeWebpackConfig(config, env), (err, stats) => {
         // require('fs').writeFileSync('stats.json', JSON.stringify(stats.toJson()));
+
+        console.log(stats.toString({
+            colors: colorsSupported,
+            chunks: false,
+            errorDetails: true
+        }));
 
         gulpRunner(config, env, () => {
             callback(err, stats);
