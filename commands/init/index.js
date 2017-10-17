ea7d0405879effaeb1a285c9b30fb8b75f941f38
Aleksey Subbotenko <asu@dvhb.ru>
2017-10-17 16:38:20 +0300

    #12 Git optional

--- commands/init/index.js
+++ commands/init/index.js
@@ -1,13 +1,11 @@
 'use strict';
 
 const Base = require('yeoman-generator').Base;
-const init = require('./generator/init');
 const install = require('./generator/install');
 const writing = require('./generator/writing');
 const prompting = require('./generator/prompting');
 
 module.exports = Base.extend({
-  init,
   prompting,
   writing,
   install
